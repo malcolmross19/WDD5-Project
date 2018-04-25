ce0cef491b60b9916d1faed2d4c103b87fdffb17
Malcolm Ross <MJRoss@student.fullsail.edu>
2018-04-25 14:40:13 -0400

    Updated App.js
    Added a timer to app that will show remaining time before answer is revealed. Added functions for changing the visibility of the timer and the track information. Added functions for getting the track currently playing on Spotify. Added function for getting an auth token from Spotify.

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SpotifyWebApi from 'spotify-web-api-js';
+import ReactCountdownClock from 'react-countdown-clock';
 
 const spotifyApi = new SpotifyWebApi();
 
 class App extends Component {
-  constructor(){
+    constructor(){
     super();
     const params = this.getHashParams();
     const token = params.access_token;
@@ -15,10 +16,16 @@ class App extends Component {
     }
     this.state = {
       loggedIn: token ? true : false,
-      nowPlaying: { name: 'Not Checked', albumArt: ''}
+      nowPlaying: { name: 'Not Checked', albumArt: ''},
+      currentCount: 5,
+      clockHidden: true,
+      songInfoHidden: true
     }
     this.getNowPlaying = this.getNowPlaying.bind(this);
     this.getHashParams = this.getHashParams.bind(this);
+    this.toggleClock = this.toggleClock.bind(this);
+    this.toggleInfo = this.toggleInfo.bind(this);
+    this.searchTracks = this.searchTracks.bind(this);
   }
 
   getNowPlaying(){
@@ -31,10 +38,22 @@ class App extends Component {
             albumArt: response.item.album.images[0].url
           }
       });
+      this.searchTracks();
     })
   }
 
-  getHashParams() {
+  searchTracks(){
+    var trackName = this.state.nowPlaying.name;
+    var searchTerm = trackName.split(' ', 1);
+    spotifyApi.searchTracks(searchTerm)
+    .then((data) => {
+      console.log('Search by "' + searchTerm + '"', data.body);
+    }, function(err) {
+      console.error(err);
+    });
+  }
+
+  getHashParams(){
     var hashParams = {};
     var e, r = /([^&;=]+)=?([^&;]*)/g,
         q = window.location.hash.substring(1);
@@ -45,22 +64,53 @@ class App extends Component {
     }
     return hashParams;
   }
+
+  toggleClock(){
+    this.setState({
+      clockHidden: !this.state.clockHidden
+    });
+  }
+
+  toggleInfo(){
+    this.toggleClock();
+    this.setState({
+      songInfoHidden: !this.state.songInfoHidden
+    });
+  }
+
+  buttonClick(e){
+    this.getNowPlaying();
+    this.toggleClock();
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Login to Spotify to use Name That Tune</h1>
         <a href="http://localhost:8888">Login to Spotify</a>
-        <div>
-          Now Playing: { this.state.nowPlaying.name }
+          <div>
+            {this.state.loggedIn && !this.state.clockHidden &&
+              <ReactCountdownClock
+                seconds={5}
+                color='#34E543'
+                alpha={0.9}
+                size={720}
+                onComplete={() => this.toggleInfo()}
+              />}
+            {this.state.loggedIn && !this.state.songInfoHidden &&
+              <div>
+                Now Playing: { this.state.nowPlaying.name }
+              </div>}
+              {this.state.loggedIn && !this.state.songInfoHidden &&
+              <div>
+                <img src={this.state.nowPlaying.albumArt} style={{ height: 650 }}/>
+              </div>}
+            {this.state.loggedIn &&
+              <button onClick={() => this.buttonClick()}>
+                Check Now Playing
+              </button>
+            }
         </div>
-        <div>
-          <img src={this.state.nowPlaying.albumArt} style={{ height: 650 }}/>
-        </div>
-        {this.state.loggedIn &&
-          <button onClick={() => this.getNowPlaying()}>
-            Check Now Playing
-          </button>
-        }
       </div>
     );
   }
